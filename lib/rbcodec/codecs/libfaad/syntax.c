2b46cdca2c3d6598f89d15577122fbce45d9ab02
roman.artiukhin <bahusdrive@gmail.com>
2023-09-06 15:37:43 +0300

    Codecs: mp4: Skip single lfe channel
    Change-Id: I9b0d35480448cbacfa15327dae7037a836cd5e59

--- lib/rbcodec/codecs/libfaad/syntax.c
+++ lib/rbcodec/codecs/libfaad/syntax.c
@@ -323,17 +323,6 @@ static void decode_sce_lfe(NeAACDecHandle hDecoder,
     uint8_t channels = hDecoder->fr_channels;
     uint8_t tag = 0;
 
-    if (channels+1 > MAX_CHANNELS)
-    {
-        hInfo->error = 12;
-        return;
-    }
-    if (hDecoder->fr_ch_ele+1 > MAX_SYNTAX_ELEMENTS)
-    {
-        hInfo->error = 13;
-        return;
-    }
-
     /* for SCE hDecoder->element_output_channels[] is not set here because this
        can become 2 when some form of Parametric Stereo coding is used
     */
@@ -343,10 +332,20 @@ static void decode_sce_lfe(NeAACDecHandle hDecoder,
 
     /* decode the element */
     hInfo->error = single_lfe_channel_element(hDecoder, ld, channels, &tag);
-
+    uint8_t real_channels = hDecoder->ps_used[hDecoder->fr_ch_ele] ? 2 : 1;
     /* map output channels position to internal data channels */
-    if (hDecoder->element_output_channels[hDecoder->fr_ch_ele] == 2)
+    if (real_channels == 2)
     {
+        if (channels+1 > MAX_CHANNELS)
+        {
+            hInfo->error = 12;
+            return;
+        }
+        if (hDecoder->fr_ch_ele+1 > MAX_SYNTAX_ELEMENTS)
+        {
+            hInfo->error = 13;
+            return;
+        }
         /* this might be faulty when pce_set is true */
         hDecoder->internal_channel[channels] = channels;
         hDecoder->internal_channel[channels+1] = channels+1;
@@ -355,6 +354,8 @@ static void decode_sce_lfe(NeAACDecHandle hDecoder,
             hDecoder->internal_channel[hDecoder->pce.sce_channel[tag]] = channels;
         else
             hDecoder->internal_channel[channels] = channels;
+        // rockbox: Ignore single lfe channel 
+        return;
     }
 
     hDecoder->fr_channels += hDecoder->element_output_channels[hDecoder->fr_ch_ele];
