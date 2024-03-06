7b394df8459d230a7d782ae571ef73d49f5fa582
Roman Artiukhin <bahusdrive@gmail.com>
2024-03-06 18:22:42 +0200

    Option to resume from recent bookmark
    Change-Id: I23443cde26d259e9b05f5e268c123e42af169863

--- apps/playlist.c
+++ apps/playlist.c
@@ -605,7 +605,11 @@ static ssize_t format_track_path(char *dest, char *src, int buf_length,
 static void new_playlist_unlocked(struct playlist_info* playlist,
                                   const char *dir, const char *file)
 {
-    empty_playlist_unlocked(playlist, false);
+    const char *fileused = file;
+    const char *dirused = dir;
+
+    bool dirPlay = !fileused &&  dirused;
+    empty_playlist_unlocked(playlist, dirPlay && global_settings.resume_from_recent_bookmark && global_settings.usemrb);
 
     /* enable dirplay for the current playlist if there's a DIR but no FILE */
     if (!file && dir && playlist == &current_playlist)
@@ -3585,6 +3589,11 @@ int playlist_set_current(struct playlist_info* playlist)
 {
     int result = -1;
 
+//    if(global_settings.resume_from_recent_bookmark && global_settings.usemrb)
+//    {
+//        return result;
+//    }
+
     if (!playlist || (check_control(playlist) < 0))
     {
         playlist_close(playlist);
@@ -3759,6 +3768,11 @@ void playlist_sync(struct playlist_info* playlist)
         audio_flush_and_reload_tracks();
 }
 
+bool playlist_resume_from_recent_bookmark(void)
+{
+    return global_settings.resume_from_recent_bookmark && global_settings.usemrb && !playlist_modified(NULL);
+}
+
 /* Update resume info for current playing song.  Returns -1 on error. */
 int playlist_update_resume_info(const struct mp3entry* id3)
 {
@@ -3780,7 +3794,8 @@ int playlist_update_resume_info(const struct mp3entry* id3)
             global_status.resume_elapsed = id3->elapsed;
             global_status.resume_offset = id3->offset;
             global_status.resume_modified = pl_modified;
-            status_save(false);
+            if (!playlist_resume_from_recent_bookmark())
+                status_save(false);
         }
     }
     else
@@ -3790,7 +3805,8 @@ int playlist_update_resume_info(const struct mp3entry* id3)
         global_status.resume_elapsed = -1;
         global_status.resume_offset = -1;
         global_status.resume_modified = false;
-        status_save(true);
+        if (!playlist_resume_from_recent_bookmark())
+            status_save(true);
         return -1;
     }
 
