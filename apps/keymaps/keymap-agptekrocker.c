b9a2b1d6789ba739fb59717a612b40070a6fa8c8
Roman Artiukhin <bahusdrive@gmail.com>
2024-06-16 20:39:22 +0300

    Rocker keymap: adjustments
    Change-Id: Ibf7eb7bf390bac05aad5eb615bedaa4189734a31

--- apps/keymaps/keymap-agptekrocker.c
+++ apps/keymaps/keymap-agptekrocker.c
@@ -40,7 +40,7 @@ static const struct button_mapping button_context_standard[]  = {
     { ACTION_STD_OK,         BUTTON_RIGHT,                BUTTON_NONE },
     { ACTION_STD_CANCEL,     BUTTON_LEFT,                 BUTTON_NONE },
     { ACTION_STD_CONTEXT,    BUTTON_SELECT|BUTTON_REPEAT, BUTTON_SELECT },
-    { ACTION_STD_MENU,       BUTTON_POWER,                BUTTON_NONE },
+    { ACTION_STD_MENU,       BUTTON_POWER|BUTTON_REL,                BUTTON_POWER },
 
 /*    ACTION_STD_QUICKSCREEN,
  *    ACTION_STD_KEYLOCK
@@ -53,17 +53,10 @@ static const struct button_mapping button_context_standard[]  = {
     LAST_ITEM_IN_LIST
 }; /* button_context_standard */
 
-
-static const struct button_mapping button_context_mainmenu[]  = {
-    { ACTION_TREE_WPS,       BUTTON_POWER,                  BUTTON_NONE },
-
-    LAST_ITEM_IN_LIST__NEXTLIST(CONTEXT_TREE),
-}; /* button_context_mainmenu as with sansa clip mapping - "back" button returns you to WPS */
-
 static const struct button_mapping button_context_wps[]  = {
         { ACTION_WPS_BROWSE,     BUTTON_UP|BUTTON_REL,        BUTTON_UP },
-        { ACTION_WPS_STOP,     BUTTON_POWER|BUTTON_REL,     BUTTON_POWER },
-
+        { ACTION_WPS_STOP,     BUTTON_POWER|BUTTON_REPEAT,     BUTTON_POWER },
+        { ACTION_WPS_MENU,       BUTTON_POWER|BUTTON_REL,  BUTTON_POWER },
         { ACTION_WPS_SKIPPREV, BUTTON_LEFT|BUTTON_REL,    BUTTON_LEFT },
         { ACTION_WPS_SEEKBACK, BUTTON_LEFT|BUTTON_REPEAT, BUTTON_NONE },
         { ACTION_WPS_STOPSEEK, BUTTON_LEFT|BUTTON_REL,    BUTTON_LEFT|BUTTON_REPEAT },
@@ -82,12 +75,9 @@ static const struct button_mapping button_context_wps[]  = {
         { ACTION_WPS_VOLDOWN,   BUTTON_VOLDOWN|BUTTON_REPEAT,    BUTTON_NONE },
 
         { ACTION_WPS_PLAY,        BUTTON_SELECT|BUTTON_REL,     BUTTON_SELECT },
-        { ACTION_WPS_QUICKSCREEN,       BUTTON_SELECT|BUTTON_REPEAT,  BUTTON_SELECT },
+        { ACTION_WPS_CONTEXT,       BUTTON_SELECT|BUTTON_REPEAT,  BUTTON_SELECT },
 
         { ACTION_WPS_QUICKSCREEN,   BUTTON_DOWN|BUTTON_REPEAT,    BUTTON_DOWN },
-        //{ ACTION_WPS_MENU,          BUTTON_HOME|BUTTON_REL,      BUTTON_HOME },
-
-        { ACTION_WPS_CONTEXT,   BUTTON_UP|BUTTON_REPEAT,        BUTTON_NONE },
         { ACTION_WPS_ID3SCREEN,     BUTTON_SELECT|BUTTON_DOWN,      BUTTON_SELECT },
 
         { ACTION_WPS_HOTKEY,        BTN_VOL_COMBO|BUTTON_REL,    BTN_VOL_COMBO},
@@ -127,6 +117,8 @@ static const struct button_mapping button_context_list[]  = {
 
 static const struct button_mapping button_context_tree[]  = {
     { ACTION_TREE_HOTKEY,     BTN_VOL_COMBO|BUTTON_REL,    BTN_VOL_COMBO},
+    { ACTION_TREE_STOP,    BUTTON_POWER|BUTTON_REPEAT,         BUTTON_POWER },
+    { ACTION_TREE_WPS,   BUTTON_POWER|BUTTON_REL,      BUTTON_POWER },
 /*    ACTION_TREE_ROOT_INIT,
  *    ACTION_TREE_PGLEFT, optional
  *    ACTION_TREE_PGRIGHT, optional
@@ -228,6 +220,18 @@ static const struct button_mapping button_context_bmark[]  = {
     LAST_ITEM_IN_LIST__NEXTLIST(CONTEXT_LIST),
 }; /* button_context_bmark */
 
+static const struct button_mapping button_context_listtree_scroll_without_combo[]  = {
+        { ACTION_NONE,              BUTTON_LEFT,                BUTTON_NONE },
+        { ACTION_STD_CANCEL,        BUTTON_LEFT|BUTTON_REL,     BUTTON_LEFT },
+        { ACTION_TREE_ROOT_INIT,    BUTTON_LEFT|BUTTON_REPEAT,  BUTTON_LEFT },
+        { ACTION_TREE_PGLEFT,       BUTTON_LEFT|BUTTON_REPEAT,  BUTTON_NONE },
+        { ACTION_NONE,              BUTTON_RIGHT,               BUTTON_NONE },
+        { ACTION_STD_OK,            BUTTON_RIGHT|BUTTON_REL,    BUTTON_RIGHT },
+        { ACTION_TREE_PGRIGHT,      BUTTON_RIGHT|BUTTON_REPEAT, BUTTON_NONE },
+
+        LAST_ITEM_IN_LIST__NEXTLIST(CONTEXT_CUSTOM|CONTEXT_TREE),
+};
+
 /* get_context_mapping returns a pointer to one of the above defined arrays depending on the context */
 const struct button_mapping* get_context_mapping(int context)
 {
@@ -239,9 +243,12 @@ const struct button_mapping* get_context_mapping(int context)
         case CONTEXT_WPS:
             return button_context_wps;
 
-        case CONTEXT_MAINMENU:
-            return button_context_mainmenu;
         case CONTEXT_TREE:
+        case CONTEXT_MAINMENU:
+            if (global_settings.hold_lr_for_scroll_in_list)
+                return button_context_listtree_scroll_without_combo;
+            /* Fallthrough */
+        case CONTEXT_CUSTOM|CONTEXT_TREE:
             return button_context_tree;
         case CONTEXT_LIST:
             return button_context_list;
