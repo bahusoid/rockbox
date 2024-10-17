410e317baf94fed7e795855961f455219fef843c
Roman Artiukhin <bahusdrive@gmail.com>
2024-10-17 20:35:26 +0300

    rocker: keymap adjustments
    Change-Id: I410ab89ae0e407e99b3e480df5de59bac12439a0

--- apps/keymaps/keymap-agptekrocker.c
+++ apps/keymaps/keymap-agptekrocker.c
@@ -89,14 +89,18 @@ static const struct button_mapping button_context_wps[]  = {
 }; /* button_context_wps */
 
 static const struct button_mapping button_context_settings[] = {
-    { ACTION_SETTINGS_INC,      BUTTON_VOLUP,                  BUTTON_NONE },
-    { ACTION_SETTINGS_INCREPEAT, BUTTON_VOLUP|BUTTON_REPEAT,   BUTTON_NONE },
-/*    ACTION_SETTINGS_INCBIGSTEP */
-    { ACTION_SETTINGS_DEC,       BUTTON_VOLDOWN,               BUTTON_NONE },
-    { ACTION_SETTINGS_DECREPEAT, BUTTON_VOLDOWN|BUTTON_REPEAT, BUTTON_NONE },
-/*    ACTION_SETTINGS_DECBIGSTEP */
-/*    ACTION_SETTINGS_SET, Used by touchscreen targets */
-    LAST_ITEM_IN_LIST__NEXTLIST(CONTEXT_STD),
+        { ACTION_STD_CANCEL,        BUTTON_POWER,               BUTTON_NONE },
+        { ACTION_SETTINGS_INC,      BUTTON_UP,                  BUTTON_NONE },
+        { ACTION_SETTINGS_INCREPEAT,BUTTON_UP|BUTTON_REPEAT,    BUTTON_NONE },
+        { ACTION_SETTINGS_DEC,      BUTTON_DOWN,                BUTTON_NONE },
+        { ACTION_SETTINGS_DECREPEAT,BUTTON_DOWN|BUTTON_REPEAT,  BUTTON_NONE },
+
+        { ACTION_STD_PREV,          BUTTON_LEFT,                BUTTON_NONE },
+        { ACTION_STD_PREVREPEAT,    BUTTON_LEFT|BUTTON_REPEAT,  BUTTON_NONE },
+        { ACTION_STD_NEXT,          BUTTON_RIGHT,               BUTTON_NONE },
+        { ACTION_STD_NEXTREPEAT,    BUTTON_RIGHT|BUTTON_REPEAT, BUTTON_NONE },
+
+        LAST_ITEM_IN_LIST__NEXTLIST(CONTEXT_STD),
 }; /* button_context_settings */
 
 static const struct button_mapping button_context_settings_right_is_inc[]  = {
