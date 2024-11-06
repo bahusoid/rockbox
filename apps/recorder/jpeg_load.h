1142f96154642e5d63ec57407bcc51fb8cc59692
Roman Artiukhin <bahusdrive@gmail.com>
2024-11-06 22:28:24 +0200

    metadata: mp3: Support unsynchronized jpeg album art
    Change-Id: I1e2ca4ae0aa967f7e80142a04c9a7d99e38e68b2

--- apps/recorder/jpeg_load.h
+++ apps/recorder/jpeg_load.h
@@ -67,11 +67,10 @@ int clip_jpeg_file(const char* filename,
  * read embedded jpeg files as above. Needs an open file descripter, and
  * assumes the caller has lseek()'d to the start of the jpeg blob
  **/
-int clip_jpeg_fd(int fd,
+int clip_jpeg_fd(int fd, bool unsync,
                  unsigned long jpeg_size,
                  struct bitmap *bm,
                  int maxsize,
                  int format,
                  const struct custom_format *cformat);
-
 #endif /* _JPEG_JPEG_DECODER_H */
