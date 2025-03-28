92e93d89e40b3db53802a43f9c7f023aeafd54bf
Roman Artiukhin <bahusdrive@gmail.com>
2025-03-28 13:38:07 +0200

    codec: opus: Comment large stack allocations log
    Change-Id: Ic4a878f1c085fbb4b1867b2bd1c9855af97e6807

--- lib/rbcodec/codecs/libopus/celt/stack_alloc.h
+++ lib/rbcodec/codecs/libopus/celt/stack_alloc.h
@@ -93,7 +93,7 @@
 #define VARDECL(type, var)
 #define ALLOCH(var, size, type) type* var = _ogg_malloc((size)*sizeof(type));
 #define ALLOC(var, size, type) type var[size]; \
-if((size)*sizeof(type) > 1000) {DEBUGF("Name: %s, size: %ld type: %s \n", #var, (size)*sizeof(type), #type);}                                       
+//if((size)*sizeof(type) > 1000) {\DEBUGF("Name: %s, size: %ld type: %s \n", #var, (size)*sizeof(type), #type);}                                       
 #define SAVE_STACK
 #define RESTORE_STACK
 #define ALLOC_STACK
