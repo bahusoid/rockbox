bd88580d261c3d8b1abbc3203be372f96918bf62
Roman Artiukhin <bahusdrive@gmail.com>
2025-05-22 15:26:54 +0300

    Codecs: mp3: Move strip tags to codec
    Another fix for FS #13626
    
    Change-Id: Ife5f65539457559fd908d0048d4df94649b0b894

--- lib/rbcodec/codecs/codecs.h
+++ lib/rbcodec/codecs/codecs.h
@@ -152,6 +152,7 @@ struct codec_api {
     long (*get_command)(intptr_t *param);
     /* Determine whether the track should be looped, if applicable. */
     bool (*loop_track)(void);
+    void (*strip_filesize)(off_t value);
 
     /* kernel/ system */
 #if defined(CPU_ARM) && CONFIG_PLATFORM & PLATFORM_NATIVE
