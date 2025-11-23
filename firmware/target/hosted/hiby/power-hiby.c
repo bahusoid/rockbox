8732408421465819f6334852dfba08619d4517d5
Marc Aarts <marc.aarts@gmail.com>
2025-11-23 20:47:19 +0100

    Rockbox for the HiBy R3 Pro II/R1
    Original author Melissa Autumn (https://codeberg.org/oopsallnaps/rockbox-hibyos) with contributions from Marc Aarts.
    
    Adaptation to Rockbox standards by Marc Aarts
    
    Change-Id: I09e5af7ba0a75c648e4b9fd424badc2d3665c943

---
+++ firmware/target/hosted/hiby/power-hiby.c
@@ -0,0 +1,67 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2017 by Marcin Bukat
+ * Copyright (C) 2025 by Melissa Autumn
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "system.h"
+#include "power-hiby.h"
+#include "power.h"
+#include "panic.h"
+#include "sysfs.h"
+
+const char * const sysfs_bat_voltage =
+"/sys/class/power_supply/battery/voltage_now";
+
+
+const char * const sysfs_bat_capacity =
+"/sys/class/power_supply/battery/capacity";
+
+const char * const sysfs_bat_tte =
+"/sys/class/power_supply/battery/time_to_empty_now";
+
+unsigned int hiby_power_get_battery_voltage(void)
+{
+    int battery_voltage;
+    sysfs_get_int(sysfs_bat_voltage, &battery_voltage);
+
+    return battery_voltage/1000;
+}
+
+
+unsigned int hiby_power_get_battery_capacity(void)
+{
+    int battery_capacity;
+    sysfs_get_int(sysfs_bat_capacity, &battery_capacity);
+
+    return battery_capacity;
+}
+
+/* Get battery time to empty (now) */
+unsigned int hiby_power_get_battery_tte(void)
+{
+    int battery_tte;
+    sysfs_get_int(sysfs_bat_tte, &battery_tte);
+
+    return battery_tte;
+}
---
+++ firmware/target/hosted/hiby/power-hiby.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *             __________               __   ___.
+ *   Open      \______   \ ____   ____ |  | _\_ |__   _______  ___
+ *   Source     |       _//  _ \_/ ___\|  |/ /| __ \ /  _ \  \/  /
+ *   Jukebox    |    |   (  <_> )  \___|    < | \_\ (  <_> > <  <
+ *   Firmware   |____|_  /\____/ \___  >__|_ \|___  /\____/__/\_ \
+ *                     \/            \/     \/    \/            \/
+ *
+ * Copyright (C) 2017 by Marcin Bukat
+ * Copyright (C) 2025 by Melissa Autumn
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ****************************************************************************/
+#ifndef _POWER_HIBY_H_
+#define _POWER_HIBY_H_
+
+#include <stdbool.h>
+#include "config.h"
+
+unsigned int hiby_power_get_battery_voltage(void);
+unsigned int hiby_power_get_battery_capacity(void);
+unsigned int hiby_power_get_battery_tte(void);
+
+#endif /* _POWER_HIBY_H_ */
