e36389410efb3368ec5981f965b3873523d372ab
Solomon Peachy <pizza@shaftnet.org>
2026-01-13 08:04:47 -0500

    settings:  double internal size of "path list" from 80 to 160 bytes
     * Autoresume path list
     * Database scan path list
    
    Gives us a bit more headroom
    
    Change-Id: Icb78d6f46dd39658334ddc3d8dc44863b0e138b4
    (cherry picked from commit 1c429e2209508acd954b31b4b3c655cb2cf7bbc2)

--- apps/settings.h
+++ apps/settings.h
@@ -42,6 +42,7 @@ struct opt_items {
 /** Setting values defines **/
 #define MAX_FILENAME 32
 #define MAX_PATHNAME 80
+#define MAX_PATHLIST (MAX_PATHNAME*2)
 
 /* The values are assigned to the enums so that they correspond to */
 /* setting values in settings_list.c                               */
@@ -634,9 +635,9 @@ struct user_settings
     //bool alt_settings_enable;   /* enable auto-resume feature? */
     int autoresume_automatic; /* resume next track? 0=never, 1=always,
                                  2=custom */
-    unsigned char autoresume_paths[MAX_PATHNAME+1]; /* colon-separated list */
+    unsigned char autoresume_paths[MAX_PATHLIST+1]; /* colon-separated list */
     bool runtimedb;           /* runtime database active? */
-    unsigned char tagcache_scan_paths[MAX_PATHNAME+1];
+    unsigned char tagcache_scan_paths[MAX_PATHLIST+1];
     unsigned char tagcache_db_path[MAX_PATHNAME+1];
 #endif /* HAVE_TAGCACHE */
     bool alt_settings_enable; /* resume next track? 0=no, 1=custom */
