7f2b6962f9cc650ef9aca5bbb7daf2fbabd071f9
Roman Artiukhin <bahusdrive@gmail.com>
2026-01-17 19:46:40 +0200

    hiby: Try to disable touch when screen is OFF
    Change-Id: I2e26d43d335de8834f02277c38a59c852790f364

--- firmware/target/hosted/backlight-unix.c
+++ firmware/target/hosted/backlight-unix.c
@@ -31,6 +31,10 @@
 #include "sysfs.h"
 #include "panic.h"
 #include "lcd.h"
+#ifdef HAVE_TOUCHSCREEN
+#include <sys/ioctl.h>
+#include <linux/input.h>
+#endif
 
 #if defined(BACKLIGHT_RG_NANO)
 static const char * const sysfs_bl_brightness =
@@ -52,6 +56,10 @@ static const char * const sysfs_bl_power =
     "/sys/class/backlight/pwm-backlight.0/bl_power";
 #endif
 
+#ifdef HAVE_TOUCHSCREEN
+static int touch_fd = -1;
+#endif
+
 bool backlight_hw_init(void)
 {
     backlight_hw_on();
@@ -62,6 +70,12 @@ bool backlight_hw_init(void)
     buttonlight_hw_brightness(DEFAULT_BRIGHTNESS_SETTING);
 #endif
 #endif
+
+#ifdef HAVE_TOUCHSCREEN
+    if (touch_fd < 0)
+        touch_fd = open("/dev/input/event1", O_RDONLY | O_NONBLOCK);
+#endif
+
     return true;
 }
 
@@ -80,6 +94,11 @@ void backlight_hw_on(void)
 #endif
         last_bl = BACKLIGHT_POWER_ON;
         sysfs_set_int(sysfs_bl_power, last_bl);
+#ifdef HAVE_TOUCHSCREEN
+        /* Added Touch Unlock */
+        if (touch_fd >= 0)
+            ioctl(touch_fd, EVIOCGRAB, (void*)0);
+#endif
     }
 }
 
@@ -90,6 +109,10 @@ void backlight_hw_off(void)
         sysfs_set_int(sysfs_bl_power, last_bl);
 #ifdef HAVE_LCD_ENABLE
         lcd_enable(false);
+#endif
+#ifdef HAVE_TOUCHSCREEN
+        if (touch_fd >= 0)
+            ioctl(touch_fd, EVIOCGRAB, (void*)1);
 #endif
     }
 }
