1ba3baedd51c31e9b1d690c259e8f47d7b9e147b
Roman Artiukhin <bahusdrive@gmail.com>
2026-08-02 17:02:36 +0300

    hiby: bluetooth: clean up
    Change-Id: I300fd2cd5bc1568d0f4fb0a3245b17cff0f47d49

--- apps/hiby_bluetooth.h
+++ apps/hiby_bluetooth.h
@@ -6,6 +6,9 @@
 int hiby_bluetooth_menu(void);
 
 #if defined(HAVE_HIBY_BLUETOOTH)
+bool bt_is_enabled_fast(void);
+bool bt_disable(void);
+bool bt_enable(void);
 bool bt_is_connected_fast(void);
 bool bt_autoconnection_route_to_bluetooth(char active_mac[18], bool bt_on);
 void bt_route_to_local(void);
