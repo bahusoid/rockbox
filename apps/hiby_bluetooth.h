b4cc1c89e60021c23991158599917f9376e48049
Roman Artiukhin <bahusdrive@gmail.com>
2026-08-05 21:48:22 +0300

    hiby: bluetooth: fix simulator build
    Change-Id: Ie649a25bbbdc13ace39f1ab202585d15fe1a28f3

--- apps/hiby_bluetooth.h
+++ apps/hiby_bluetooth.h
@@ -5,13 +5,11 @@
 
 int hiby_bluetooth_menu(void);
 
-#if defined(HAVE_HIBY_BLUETOOTH)
 bool bt_is_enabled_fast(void);
 bool bt_disable(void);
 bool bt_enable(void);
 bool bt_is_connected_fast(void);
 bool bt_autoconnection_route_to_bluetooth(char active_mac[18], bool bt_on);
 void bt_route_to_local(void);
-#endif
 
 #endif /* __HIBY_BLUETOOTH_H__ */
