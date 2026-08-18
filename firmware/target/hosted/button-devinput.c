6dac0fede8649ac14905a26f532cc4aedf5abee7
Roman Artiukhin <bahusdrive@gmail.com>
2026-08-18 11:39:03 +0300

    hiby: r3proii: invert next/prev only for built-in buttons
    Change-Id: I2915ec9ead61937e1f719a1e680838ee03c88023

--- firmware/target/hosted/button-devinput.c
+++ firmware/target/hosted/button-devinput.c
@@ -225,6 +225,16 @@ int button_read_device(BDATA)
                 if(size == (int)sizeof(event)) {
                     switch(event.type) {
                     case EV_KEY: {
+                            //Hack alert: should be moved to target button button_map?
+#ifdef HAVE_INVERTED_NEXT_BUTTON
+                            if (i < 3) //assume that's first 3 devices are built-in buttons
+                            {
+                                if (event.code == KEY_PREVIOUSSONG)
+                                    event.code = KEY_NEXTSONG;
+                                else if (event.code == KEY_NEXTSONG)
+                                    event.code = KEY_PREVIOUSSONG;
+                            }
+#endif
                         /* map linux event code to rockbox button bitmap */
                         int bmap = button_map(event.code);
 
