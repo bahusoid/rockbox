ee701d9e7536338cdd1110846980788c9b3d5ca0
Roman Artiukhin <bahusdrive@gmail.com>
2026-09-01 12:23:02 +0300

    touchscreen: lists: prevent quiting from cuesheet on long tap
    Change-Id: I85ed953d5fc8b367d8636bed21fc4dade18de32d

--- apps/gui/bitmap/list.c
+++ apps/gui/bitmap/list.c
@@ -940,7 +940,9 @@ unsigned gui_synclist_do_touchscreen(struct gui_synclist *list)
 
                 if (gevent.id == GESTURE_TAP)
                     action = ACTION_STD_OK;
-                else if (gevent.id == GESTURE_LONG_PRESS)
+                else if (gevent.id == GESTURE_LONG_PRESS
+                    //to avoid accidental quit from cuesheet
+                    && get_current_activity() != ACTIVITY_CUESHEET)
                     action = ACTION_STD_CONTEXT;
                 else
                     action = ACTION_REDRAW;
