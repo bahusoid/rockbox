c4a2dcf11a7e4ce0a3877812caea6b8f47c6608c
Roman Artiukhin <bahusdrive@gmail.com>
2026-09-02 19:31:41 +0300

    fixup! settings: change defaults
    Change-Id: I860054afc5ee53abe657e7a93dde4d19419aa998

--- apps/settings_list.c
+++ apps/settings_list.c
@@ -1500,11 +1500,8 @@ const struct settings_list settings[] = {
     INT_SETTING(F_BANFROMQS, bt_selective_softlock_actions_mask,
                 LANG_SOFTLOCK_SELECTIVE,
                 SEL_ACTION_VOL | SEL_ACTION_PLAY | SEL_ACTION_SEEK |
-                    SEL_ACTION_SKIP | SEL_ACTION_START_STOP 
-                    
-#ifdef HAVE_TOUCHSCREEN
-                    | SEL_ACTION_NOTOUCH
-#endif
+                    SEL_ACTION_SKIP | SEL_ACTION_START_STOP
+                    | SEL_ACTION_ALLNONOTIFY | SEL_ACTION_NONOTIFY
                     ,
                 "Selective Screen Lock Actions", UNIT_INT,
                 0, 2048,2, NULL, NULL, NULL),
