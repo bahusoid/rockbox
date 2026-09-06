3f796b4fbf7c8a371ed9b2e73f0511bc87bc32b1
Roman Artiukhin <bahusdrive@gmail.com>
2026-09-06 14:22:02 +0300

    Codecs: dsd64: dsf, dff and SACD .iso playback
    Stereo DSD64 .dsf playback
    Stereo uncompressed DSD64 .dff playback SACD .iso playback
    Uncompressed DSD and DST-compressed audio inside SACD ISO images
    DSF, DFF, and ISO files are visible in the file browser
    
    Patch by Martin Cai:
    https://github.com/Black0ut111/Hi-M3k-Rockbox/blob/main/patches/0041-dsd64-dsf-dff-playback.patch
    
    Change-Id: I95d520c12e4aa40d54f53e3347524e36f8aeeae3

--- lib/rbcodec/metadata/metadata_parsers.h
+++ lib/rbcodec/metadata/metadata_parsers.h
@@ -58,3 +58,4 @@ bool get_sgc_metadata(int fd, struct mp3entry* id3);
 bool get_vgm_metadata(int fd, struct mp3entry* id3);
 bool get_kss_metadata(int fd, struct mp3entry* id3);
 bool get_aac_metadata(int fd, struct mp3entry* id3);
+bool get_dsd_metadata(int fd, struct mp3entry* id3);
