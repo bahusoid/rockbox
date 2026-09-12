ce939f826525107f6f2859aa850995f94a3aa7c7
Roman Artiukhin <bahusdrive@gmail.com>
2026-09-12 15:45:08 +0300

    keymap: hibyr1: initial bluetooth keyboard support
    Change-Id: Id8058aa55d6406ab4d93f12fed6564abf36abaed

--- firmware/target/hosted/hiby/r1/button-r1.c
+++ firmware/target/hosted/hiby/r1/button-r1.c
@@ -35,7 +35,7 @@
  *   /dev/input/event4: dynamic input devices start at 4 and increment (Bluetooth AVRCP remote control)
  */
 
-int hw_button_map(int keycode)
+static int hw_button_map(int keycode)
 {
     switch(keycode)
     {
@@ -66,27 +66,44 @@ int hw_button_map(int keycode)
     }
 }
 
+static int remote_button_map(int keycode)
+{
+    switch(keycode)
+    {
+    //Map bluetooth buttons:
+    case KEY_UP:
+        return BUTTON_UP;
+    case KEY_DOWN:
+        return BUTTON_DOWN;
+    case KEY_LEFT:
+        return BUTTON_LEFT;
+    case KEY_RIGHT:
+        return BUTTON_RIGHT;
+    case KEY_SPACE:
+    case KEY_PLAY:
+    case KEY_PLAYCD:
+    case KEY_PAUSECD:
+        return BUTTON_PLAY;
+    case KEY_REWIND:
+    case KEY_PREVIOUSSONG:
+        return BUTTON_PREV;
+    case KEY_FASTFORWARD:
+    case KEY_NEXTSONG:
+        return BUTTON_NEXT;
+    case BUTTON_VOL_UP:
+        return BUTTON_VOL_UP;
+    case BUTTON_VOL_DOWN:
+        return BUTTON_VOL_DOWN;
+    default:
+        return 0;
+    }
+}
+
 int button_map_with_id(int keycode, int id)
 {
     //TODO: Decide how to handle earpod_adc
-    if (id < 4)
+    if (id < 3)
         return hw_button_map(keycode);
-    
-    switch(keycode)
-    {
-        //Map bluetooth buttons:
-        case KEY_PLAY:
-        case KEY_PLAYCD:
-        case KEY_PAUSECD:
-            return BUTTON_PLAY;
-        case KEY_REWIND:
-        case KEY_PREVIOUSSONG:
-            return BUTTON_PREV;
-        case KEY_FASTFORWARD:
-        case KEY_NEXTSONG:
-            return BUTTON_NEXT;
 
-        default:
-            return 0;
-    }
+    return remote_button_map(keycode);
 }
